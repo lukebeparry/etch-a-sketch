63afeef798fb6ebb8a6cd022ff5db5c367188f3a
lukebeparry <lukebeparry@gmail.com>
2022-07-20 07:17:18 +0100

    Generate grid size from variable

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-for (let i=0; i<16; i++) {
+let canvasSize = 20;
+document.documentElement.style.setProperty(`--canvas`, `${canvasSize}`);
+
+for (let i=0; i<(canvasSize**2); i++) { // adds a div to the grid
     const cell = document.createElement('div');
     cell.classList.add('cell');
     document.querySelector('.container').appendChild(cell);
 }
-
-const draw = document.querySelectorAll('div');
-draw.addEventListener('mouseover', () => {
-    draw.classList.add('highlight');
+const draw = document.querySelectorAll('.cell'); 
+draw.forEach((draw) => { //applies function to each node in list
+    draw.addEventListener('mouseover', () => {
+        draw.classList.add('highlight'); //adds highlight class to existing div
+    });
 });
+function resetCanvas () {
+    canvasSize = prompt("Enter size of canvas");
+}
